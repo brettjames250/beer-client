748dab5cad05e90689307f46ecf8adfccb92f970
Your Name <brettjames250@gmail.com>
2021-11-10 11:02:34 +0000

    adding missed files

--- src/main/java/com/springmicroservices/breweryclient/web/client/BreweryClient.java
+++ src/main/java/com/springmicroservices/breweryclient/web/client/BreweryClient.java
@@ -1,14 +1,28 @@
 package com.springmicroservices.breweryclient.web.client;
 
+import com.springmicroservices.breweryclient.web.model.BeerDto;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
 
 @Component
 @ConfigurationProperties(value = "beer.project", ignoreUnknownFields = false)
 public class BreweryClient {
 
     private String apiHost;
-    public final String BEER_PATH_V1 = "/api/v1/beer";
+    public final String BEER_PATH_V1 = "/api/v1/beer/";
+    private final RestTemplate restTemplate;
+
+    public BreweryClient(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public BeerDto getBeerById(UUID beerId){
+        return restTemplate.getForObject(apiHost + BEER_PATH_V1 + beerId.toString(), BeerDto.class);
+    }
 
     public void setApiHost(String apiHost){
         this.apiHost = apiHost;
