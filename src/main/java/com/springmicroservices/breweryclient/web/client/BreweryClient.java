2a4a3d63f3e1c89e9011d0b4921d7c03ff78ff6d
Your Name <brettjames250@gmail.com>
2021-11-10 10:56:09 +0000

    adding missed files

--- src/main/java/com/springmicroservices/breweryclient/web/client/BreweryClient.java
+++ src/main/java/com/springmicroservices/breweryclient/web/client/BreweryClient.java
@@ -1,2 +1,17 @@
-package com.springmicroservices.breweryclient.web.client;public class BreweryClient {
+package com.springmicroservices.breweryclient.web.client;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(value = "beer.project", ignoreUnknownFields = false)
+public class BreweryClient {
+
+    private String apiHost;
+    public final String BEER_PATH_V1 = "/api/v1/beer";
+
+    public void setApiHost(String apiHost){
+        this.apiHost = apiHost;
+    }
+
 }
